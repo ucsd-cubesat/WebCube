c09a0f169427be94fc214f13da66962a7760ca81
Christopher Madrigal <c4madrig@ucsd.edu>
2020-09-27 14:43:21 -0700

    meep

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -3,8 +3,13 @@ import { Divider, Image } from 'semantic-ui-react';
 
 function WelcomePage() {
     return (
-        <div className="ui center aligned container">
-            <Image size="medium" centered={true} src="../TritonCubed_Logo_White_RevB.png"/>
+        <div>
+            <div className="ui center aligned container">
+                <Image size="medium" centered={true} src="../TritonCubed_Logo_White_RevB.png"/>
+            </div>
+            <div className="left aligned container" style={{margin: "5 em", "font-size": "50 px"}}>
+                Welcome to our front page!
+            </div>
         </div>
     );
 }
