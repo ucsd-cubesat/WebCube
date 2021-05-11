5a0d56f6ddf2781df0a0b529d1d687d5729e5b58
Christopher Madrigal <chrisjmadrigal@gmail.com>
2021-05-10 18:25:24 -0700

    Using class state to mutate demo page

--- src/pages/DemoPage.js
+++ src/pages/DemoPage.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
+class DemoPage extends Component {
 
-// Servlet is accessible from localhost:8080/launchapi/db
+    constructor(props) {
+        super(props);
 
-let data;
-
-axios.get("http://localhost:8080/launchapi/db", {
-    params: {
-        utc: 1620011906531
+        // init state here if needed
+        this._isMounted = false;
+        this.state = {
+            data: []
+        };
     }
-}).then(response => {
-    console.log(response.data)
-    data = response.data
-})
 
-function DemoPage() {
-    return (
-        <div className="ui left aligned container">
-            <p style={{"fontSize": "1.5em"}}>{data}</p>
-        </div>
-    );
+    componentDidMount() {
+        this._isMounted = true;
+        this.updateData()
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    updateData() {
+        axios.get("http://localhost:8080/launchapi/db", {
+            params: {
+                utc: 1620011906531
+            }
+        }).then(response => {
+            console.log(response.data)
+            this.setState({
+                data: response.data
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="ui left aligned container">
+                <p style={{"fontSize": "1.5em"}}>{this.state.data}</p>
+            </div>
+        );
+    }
 }
 
 export default DemoPage;
