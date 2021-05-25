2be1f5998c1494f903faa55172d312ae53e98f08
Christopher Madrigal <chrisjmadrigal@gmail.com>
2021-05-24 20:19:56 -0700

    update packages

--- src/components/VectorPlot.js
+++ src/components/VectorPlot.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { useMemo, useCallback } from 'react'
+import * as THREE from 'three'
+import ReactDOM from 'react-dom'
+import { Canvas, extend, useThree, useResource } from 'react-three-fiber'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
+import './styles.css'
 
-class VectorPlot extends Component {
+extend({ DragControls })
 
-    constructor(props) {
-        super(props)
-    }
-
-
-    render() {
-        return (
-            <canvas width={this.props.width} height={this.props.height} />
-        )
-    }
+function Scene() {
+    const { camera, gl } = useThree()
+    const [ref, object] = useResource()
+    const points = useMemo(() => [new THREE.Vector3(-10, 0, 0), new THREE.Vector3(0, 10, 0), new THREE.Vector3(10, 0, 0)], [])
+    const onUpdate = useCallback(self => self.setFromPoints(points), [points])
+    return (
+        <>
+            <line position={[0, -2.5, -10]} ref={ref}>
+                <bufferGeometry attach="geometry" onUpdate={onUpdate} />
+                <lineBasicMaterial attach="material" color={'#9c88ff'} linewidth={10} linecap={'round'} linejoin={'round'} />
+            </line>
+            <dragControls args={[[object], camera, gl.domElement]} />
+        </>
+    )
 }
-
-export default VectorPlot;
--- src/pages/DemoPage.tsx
+++ src/pages/DemoPage.tsx
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import axios from "axios";
 
-import {Dimmer, Loader, Segment} from 'semantic-ui-react';
+import {Dimmer, Loader} from 'semantic-ui-react'
 import {Map, Marker, Point} from 'pigeon-maps'
+import { Canvas } from '@react-three/fiber'
+// @ts-ignore
+import {LineChart} from 'react-chartkick'
+import 'chartkick/chart.js'
+import VectorPlot from "../components/VectorPlot"
 
 interface Payload {
-    temp: number
+    utc: number
+    accel_x: number
+    accel_y: number
+    accel_z: number
+    gyro_x: number
+    gyro_y: number
+    gyro_z: number
+    mag_x: number
+    mag_y: number
+    mag_z: number
+    temp_C: number
+
+    // Needed in the future
     latitude: number
     longitude: number
-    utc: number
+    altitude: number
 }
 
 interface DemoPageState {
@@ -66,25 +83,36 @@ class DemoPage extends Component<any, DemoPageState> {
             return <Marker key={"marker_"+e.utc} width={width} color={color} anchor={[e.latitude, e.longitude]} />
         })
 
+        const tempData: any = {}
+        packets.forEach(e => {
+            let date = new Date(e.utc);
+            tempData[date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()] = e.temp_C
+        })
+
         return (
             <div style={{backgroundColor: 'black', height: '93.4%', width: '100%'}}>
-                <Dimmer active={packets.length == 0}>
-                    <Loader size={"huge"}>Loading...</Loader>
-                </Dimmer>
                 <div style={{
                     height: '96%'
                 }}>
                     <Map center={center} defaultZoom={13}>
                         {markers}
-                        <div style={{position: 'relative', zIndex: 100,
-                                     top: '3em', left: '3em', width: '20em', height: '15em',
-                                     backgroundColor: 'white', color: 'black'}}>
+                        <Dimmer active={packets.length === 0}>
+                            <Loader size={"huge"}>Loading...</Loader>
+                        </Dimmer>
+                        <div  style={{position: 'relative', zIndex: 100,
+                                     top: '3em', left: '3em', width: '25em', height: '20em',
+                                     backgroundColor: 'white'}}>
+                            <LineChart data={tempData} title={"temperature"} height={'100%'} suffix={'°C'} />
                         </div>
+                        <Canvas style={{position: 'relative', zIndex: 200,
+                            top: '17em', left: '3em', width: '10em', height: '10em',
+                            backgroundColor: 'black'}}>
+                            <ambientLight intensity={1}/>
+                            <VectorPlot x={1} y={1} z={1} />
+                        </Canvas>
                     </Map>
                 </div>
-                <div style={{
-                    padding: '0.5em'
-                }}>
+                <div style={{padding: '0.5em'}}>
                     <p><b>Last fix: {lastFix}</b></p>
                 </div>
             </div>
